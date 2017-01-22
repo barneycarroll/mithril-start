ef3321d19d3eca5590a44ab6b38b72d407ad0ffe
Oliver Hunt <oliver@oliverhunt.net>
2017-01-22 01:40:47 +0000

    es6 testing working with coverage using istanbul/nyc

---
+++ src/services/function-test.js
@@ -0,0 +1,11 @@
+function functionTest(argument) {
+  function test (bob){
+    return bob
+  }
+  function edge(bab){
+    console.log(bab)
+  }
+  return argument
+}
+
+export default functionTest
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,8 +1,13 @@
 import o from 'ospec'
+import functionTest from '../services/function-test'
 
 o('addition', function () {
   o(1 + 1).equals(2)
 })
 o('subtraction', function () {
   o(1 - 1).notEquals(2)
+})
+
+o('import works', function () {
+  o(functionTest("lol")).equals("lol")
 })
