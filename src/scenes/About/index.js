570063eb10a933701cf4a21e4ea1b1ecadcc15bf
Oliver Hunt <oliver@oliverhunt.net>
2017-01-26 12:50:31 +0000

    first working copy of bundle splitting working with isomorphic

--- src/scenes/About/index.js
+++ src/scenes/About/index.js
@@ -1,13 +1,22 @@
 import m from 'mithril'
-import about from './about'
 import layout from '../../components/layout'
+
+if (typeof require.ensure !== 'function') require.ensure = require('isomorphic-ensure')({dirname: __dirname})
 
 export default {
   onmatch () {
-
+    var resolver = this
+    return new Promise(function(resolve) {
+      require.ensure(['./about.js'], function(require) {
+        resolver.about = require('./about.js').default;
+        console.log(resolver.home)
+        resolve()
+      })
+    })
   },
   render (vnode) {
+    var resolver = this
     this.title = 'About - Mithril'
-    return m(layout, vnode.attrs, m(about, vnode.attrs))
+    return m(layout, vnode.attrs, m(resolver.about, vnode.attrs))
   }
 }
--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,14 +1,22 @@
 import m from 'mithril'
-import home from './home'
 import layout from '../../components/layout'
+
+if (typeof require.ensure !== 'function') require.ensure = require('isomorphic-ensure')({dirname: __dirname})
 
 export default {
   onmatch () {
-
+    var resolver = this
+    return new Promise(function(resolve) {
+      require.ensure(['./home.js'], function(require) {
+        resolver.home = require('./home.js').default;
+        resolve()
+      })
+    })
   },
   render (vnode) {
+    var resolver = this
     this.title = 'Home - Mithril'
     this.description = 'test the meta description'
-    return m(layout, vnode.attrs, m(home, vnode.attrs))
+    return m(layout, vnode.attrs, m(resolver.home, vnode.attrs))
   }
 }
