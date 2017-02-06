6b250a1b458cbe8e3598b4959e9f931617a8660e
Oliver Hunt <oliver@oliverhunt.net>
2017-02-06 13:13:37 +0000

    tweaks to dist server, compression, wildcard static route etc

--- build/distServer.js
+++ build/distServer.js
@@ -1,18 +1,33 @@
-import bs from 'browser-sync'
+import './mithrilSetup'
+import path from 'path'
+import fs from 'fs'
 import compression from 'compression'
+import csshook from 'css-modules-require-hook/preset'
+import express from 'express'
+import routes from '../src/routes'
+import render from './render'
 
-var browserSync = bs.create()
+const port = 3000
+const app = express()
 
-browserSync.init({
-  server: 'dist',
-  open: true,
-  logFileChanges: false,
-  middleware: [
-    compression()
-  ],
-  plugins: ['bs-fullscreen-message'],
-  files: [
-    'app/css/*.css',
-    'app/*.html'
-  ]
+function getHtml () {
+  return fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), "utf8");
+}
+
+app.use(render({
+  html: getHtml,
+  routes: routes
+}))
+
+app.use(compression())
+
+var assetsDir = path.resolve(__dirname, '../dist/')
+
+app.use(express.static(assetsDir));
+app.use((req, res) => res.sendFile(`${assetsDir}/index.html`))
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  }
 })
